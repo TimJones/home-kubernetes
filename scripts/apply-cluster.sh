03e3224efe1309a09b917c3dbfb683a43f2b91f8
Tim Jones <tim@freakinabox.com>
2018-02-19 21:52:57 +0100

    Properly wait for Tiller to be available before applying Helm charts

--- scripts/apply-cluster.sh
+++ scripts/apply-cluster.sh
@@ -5,6 +5,11 @@ kubectl --namespace kube-system get serviceaccount tiller &> /dev/null || kubect
 kubectl get clusterrolebinding tiller &> /dev/null || kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 helm init --upgrade --service-account tiller
 
-sleep 5
+# Wait for Tiller to be up-to-date
+echo -n "Waiting for tiller to be ready"
+until [ "$(kubectl --namespace kube-system get deployment tiller-deploy -o jsonpath='{.status.availableReplicas}')" == "1" ]; do
+  echo -n "."
+  sleep 5
+done
 
 helmfile sync
