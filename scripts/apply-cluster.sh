4a2f1bcb5414e42ed215f8d4e27723f37e4e6f68
Tim Jones <tim@freakinabox.com>
2018-02-19 21:09:45 +0100

    Give Tiller cluster-admin permissions

--- scripts/apply-cluster.sh
+++ scripts/apply-cluster.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-helm init --upgrade
+# Set up Tiller
+kubectl --namespace kube-system get serviceaccount tiller &> /dev/null || kubectl --namespace kube-system create serviceaccount tiller
+kubectl get clusterrolebinding tiller &> /dev/null || kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+helm init --upgrade --service-account tiller
 
 sleep 5
 
